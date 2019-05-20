78eed3b298e6b3736f179350c86550db14271eda
Ryan <rjoaquim95@gmail.com>
2019-05-20 14:51:04 -0400

    added validation

--- Disaster/src/app/contractor-tc-submit/contractor-tc-submit.component.ts
+++ Disaster/src/app/contractor-tc-submit/contractor-tc-submit.component.ts
@@ -13,6 +13,9 @@ import { Jobs } from '../job'
   styleUrls: ['./contractor-tc-submit.component.css']
 })
 export class ContractorTcSubmitComponent implements OnInit {
+  public submitted = false;
+  public tooManyJobHours
+  public tooManyMachineHours
   public timecardForm: FormGroup;
   public time: number = 0
   public total: number = 0
@@ -20,14 +23,16 @@ export class ContractorTcSubmitComponent implements OnInit {
   public machineRents: Number[] = [];
   public jobs: Jobs[];
   public jobRents: Number[] = [];
+  public jobMax: Number[] = [];
+  public machineMax: Number[] = [];
+  public machinevalid: boolean[] = [];
+  public jobvalid: boolean[] = [];
   errorMsg = "";
   constructor(private fb: FormBuilder, private _TimecardService: TimecardService, private router: Router, private machineService: MachineService, private jobService: jobsService) { }
   ngOnInit() {
     this.timecardForm = this.fb.group({
       sitecode: ['', [Validators.required, Validators.minLength(3)]],
-      contractor: [""],
-      totalHours: [""],
-      totalAmount: [],
+      contractor: ["", [Validators.required]],
       machines: this.fb.array([this.initMachine()]),
       jobs: this.fb.array([this.initJob()])
     })
@@ -38,15 +43,15 @@ export class ContractorTcSubmitComponent implements OnInit {
   }
   initMachine() {
     return this.fb.group({
-      machineCode: [""],
-      hoursUsed: [],
+      machineCode: ["",[Validators.required]],
+      hoursUsed: ["",[Validators.required,Validators.pattern("^[0-9]*$")]],
       total:[]
     });
   }
   initJob() {
     return this.fb.group({
-      jobCode: [],
-      hoursWorked: [],
+      jobCode: ["",[Validators.required]],
+      hoursWorked: ["",[Validators.required,Validators.pattern("^[0-9]*$")]],
       total:[]
     });
   }
@@ -68,23 +73,44 @@ export class ContractorTcSubmitComponent implements OnInit {
   }
   setMachineRent(event, i) {
     this.machineRents[i] = event.rent;
+    this.machineMax[i] = event.maxHours;
     this.calculateMachineTotal(i);
   }
   calculateMachineTotal(i) {
     if (this.timecardForm.get('machines').value[i].hoursUsed != undefined && this.machineRents[i] != undefined) {
+      if(this.timecardForm.get('machines').value[i].hoursUsed>this.machineMax[i]){
+        this.machinevalid[i]=true
+      }
+      else{
+        this.machinevalid[i]=false
+      }
       this.timecardForm.get('machines').value[i].total = this.timecardForm.get('machines').value[i].hoursUsed * Number(this.machineRents[i])
     }
   }
   setJobRent(event, i) {
     this.jobRents[i] = event.rate;
+    this.jobMax[i] = event.maxHours;
     this.calculateJobTotal(i);
   }
   calculateJobTotal(i) {
     if (this.timecardForm.get('jobs').value[i].hoursWorked != undefined && this.jobRents[i] != undefined) {
+      if(this.timecardForm.get('jobs').value[i].hoursWorked>this.jobMax[i]){
+        this.jobvalid[i]=true
+      }
+      else{
+        this.jobvalid[i]=false
+      }
       this.timecardForm.get('jobs').value[i].total = this.timecardForm.get('jobs').value[i].hoursWorked * Number(this.jobRents[i])
     }
   }
   onSubmit() {
+    this.submitted = true;
+    this.tooManyMachineHours = this.machinevalid.indexOf(false)>0
+    this.tooManyJobHours = this.machinevalid.indexOf(false)>0
+    if(this.timecardForm.invalid||this.tooManyJobHours || this.tooManyMachineHours){
+      return
+    }
+    
     var sCode = this.timecardForm.value.sitecode
     var contractor = this.timecardForm.value.contractor
     for (var i = 0; i < this.timecardForm.get('jobs').value.length; i++) {
