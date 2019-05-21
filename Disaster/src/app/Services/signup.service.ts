51fac2270e7abe0b21e602f23b7d923271efe360
preethi <preethi@LENOVO-PC>
2019-05-21 06:43:50 -0400

    c5

--- Disaster/src/app/Services/signup.service.ts
+++ Disaster/src/app/Services/signup.service.ts
@@ -6,7 +6,7 @@ import { signup } from '../Models/signup';
   providedIn: 'root'
 })
 export class SignupService {
-
+ 
  private _url:string="http://localhost:4000/signup";
 
   constructor(private http: HttpClient) { }
